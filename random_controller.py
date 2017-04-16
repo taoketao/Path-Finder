b1ded2d1e2d68b0e9daa15dc6d5d1f21d2c9dada
taoketao <morgan.r.bryant@gmail.com>
2017-04-15 17:29:24 -0700

    Random agent controller implemented.

--- random_controller.py
+++ random_controller.py
@@ -6,26 +6,32 @@ from environment import *
 import tensorflow as tf
 import numpy as np
 
-''' Hyper parameters '''
-''' Setting constants '''
+''' [Helper] Constants '''
 N_ACTIONS = 4
 ACTIONS = [UDIR, RDIR, DDIR, LDIR]
+NULL_ACTION = -1;
 XDIM = 0; YDIM = 1
+LAYER_NAMES = { 0:"Agent", 1:"Goal", 2:"Immobiles", 3:"Mobiles" }
+ACTION_NAMES = { UDIR:"UDIR", DDIR:"DDIR", RDIR:"RDIR", LDIR:"LDIR" }
 
+''' Hyper parameters '''
+max_num_actions = 8;#20; # 50?
+training_episodes = 1;  # ... like an epoch
+
+''' Architecture parameters '''
 conv1_size = 64; # number of filters in conv layer #1
 conv2_size = 64; # number of filters in conv layer #2
 fc_1_size = 100; # number of outputs in dense layer #1
 fc_2_size = 4;   # number of outputs in dense layer #2
 
-def network1(object):
-    def __init__(self, gridsz):
+class network1(object):
+    def __init__(self, _env):
         ''' inputs: shape [batch, X, Y, 4-action-layers]. '''
-        self.window1 = (gridsz[XDIM]-2, gridsz[YDIM]-2)
-        self.window2 = (gridsz[XDIM]-4, gridsz[YDIM]-4)
-
-        #self.W1 = tf.Variable(tf.random_normal([conv2_size, fc_1_size])
-        #self.W2 = tf.Variable(tf.random_normal([fc_1_size, fc_2_size])
-
+        self.gridsz = _env.getGridSize()
+        self.env = _env
+        self.window1 = (self.gridsz[XDIM]-2, self.gridsz[YDIM]-2)
+        self.window2 = (self.gridsz[XDIM]-4, self.gridsz[YDIM]-4)
+        '''
         self.input_layer = tf.placeholder(tf.float32,               \
                 [None, gridsz[XDIM], gridsz[YDIM], N_ACTIONS]);
         self.l1 = tf.layers.conv2d( name = "layer1",                \
@@ -41,27 +47,63 @@ def network1(object):
         self.out = self.fc2
         # these outputs are q-value estimates.
         self.predict = tf.argmax(self.out)
+        '''
+    def update(self, cur_state, action_req, reward):
+        if reward==0: return; # an obvious optimization
+        # Q(s,a) = 
 
-def chooseRandAction(s):
-    return np.random.choice(ACTIONS);
+    def chooseRandAction(self, s):
+        return np.random.rand(N_ACTIONS);
+    def chooseNeuralAction(self, s):
+        pass # stub
+    def rewardGoalReached(self): pass
 
-
+def acceptableChoice(choice, valids):
+    return bool(valids[choice])
 
 # STUB: One iteration of a game:
 env = environment_handler(filename="./state_files/3x3-diag.txt")
 # STUB: consider actions for one state:
-s0 = env.getStartState();
-gridsize = env.getGridSize(); print gridsize
-R = network1(gridsize)
+RndNet = network1(env)
 
 init = tf.global_variables_initializer()
 with tf.Session() as sess:
-    pass
+    sess.run(init);
+    for episode in range(training_episodes):
+        cur_state = env.getStartState();
+        print "Initial state:"
+        env.displayGameState(cur_state); print ''
+        #goal_reached_bit = False
+        for nth_action in range(max_num_actions):
+
+            # no advantage function for now...
 
 
+            Q_sa_scores = RndNet.chooseRandAction(cur_state)
+            # Assume Q_sa_scores are >= 0, so that bit masking would work
+            action_choice = np.argmax(Q_sa_scores) # epsilon here
+            valid_actions = env.getActionValidities(cur_state)
+            valid_choice = acceptableChoice(action_choice, valid_actions)
+            print str(nth_action)+"th action requested:", ACTION_NAMES[action_choice],'(', action_choice,'). Valid?:', valid_choice
+            if not valid_choice:
+                action_taken = np.argmax(Q_sa_scores * valid_actions)
+                print '\tChose instead:', ACTION_NAMES[action_taken], action_taken
+            else:
+                action_taken = action_choice
+            next_state = env.performAction(cur_state, action_taken)
+            print "new state:"
+            env.displayGameState(next_state); print ''
+            reward = float(env.isGoalReached(next_state))
+            RndNet.update(cur_state, action_choice, reward)
 
-valid_actions = env.getActionValidities(s0);
+            if reward==1.0:
+                RndNet.rewardGoalReached()
+                #goal_reached_bit = True
+                print "Goal Reached!"
+                break;
 
+            cur_state = next_state
+        print '----------------------------------------'
 
 
 print "Done."
