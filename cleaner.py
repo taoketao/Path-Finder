3a6851dc579bbb0cb985b75e9372bfd22323a878
taoketao <morgan.r.bryant@gmail.com>
2017-08-02 18:00:50 +0200

    small commit: state template init ready.

--- cleaner.py
+++ cleaner.py
@@ -20,7 +20,7 @@ from experiment import *
 #    def get_tse_environment(self):
 #        env = env_h(gridsize=(9,9), 
 
-#X=0; Y=1;
+X=0; Y=1;
 #START_LOCS = [ (1,5), (5,1), (9,5), (5,9) ] # U L D R
 #FLAV_LOCS = [ (3,3), (9,3), (6,4), (4,6), (2,7), (8,7) ]
 
@@ -48,6 +48,38 @@ TEMPLATE_TSE = ''' x x x x x x x x x x x  r
                    x x x x x x x x x x x  e
         o: D, *  
                    '''
+
+''' Printing legend for condensed mode:
+    !  agent and goal       I  agent        -  immobile
+    @  goal and mobile      *  goal         o  mobile
+    <space>  empty          #  ERROR                        '''
+def print_state(start_state, mode):
+    st = start_state['state']
+    print mode+':'
+    if mode=='matrices':
+        for i in range(st.shape[-1]):
+            print st[:,:,i]
+    if mode=='condensed':
+        for y in range(st.shape[Y]):
+            for x in range(st.shape[X]):
+                if st[x,y,goalLayer] and st[x,y,agentLayer]: print '!',
+                elif st[x,y,agentLayer]: print 'I',
+                elif st[x,y,goalLayer] and st[x,y,mobileLayer]: print '@',
+                elif st[x,y,goalLayer]: print '*',
+                elif st[x,y,immobileLayer]: print '-', # '◊', 
+                elif st[x,y,mobileLayer]: print 'o',
+                elif 0==np.sum(st[x,y,:]): print ' ',
+                else: 
+                    print '#'
+                    raise Exception("Error")
+            print ''
+    print "Signal: ", start_state['signal']
+
+
+
+
+
+
 
 class ExpAPI(environment_handler3):
     def __init__(self, experiment_name, centr):
@@ -83,21 +115,32 @@ class ExpAPI(environment_handler3):
 
         start_locs = list(self.find_all(state_template, 'a'))
         goal_locs = list(self.find_all(state_template, '!'));
+        block_locs = list(self.find_all(state_template, 'x'));
+        if 'D' in state_template:
+            mobile_locs = list(self.find_all(state_template, '!'));
 
-        ry = [0,1,self.gridsize[YDIM]-2, self.gridsize[YDIM]-1]
-        rx = [0,1,self.gridsize[XDIM]-2, self.gridsize[XDIM]-1]
+        rx = [0,1,self.gridsize[X]-2, self.gridsize[X]-1]
+        ry = [0,1,self.gridsize[Y]-2, self.gridsize[Y]-1]
 
         for start_box in start_locs:
             for flav_id, flavor_loc in enumerate(goal_locs):
-                st = np.zeros( (self.gridsize[XDIM], self.gridsize[YDIM], NUM_LAYERS))
-                st[ start_box[XDIM], start_box[YDIM], agentLayer ] = 1.0
-                st[ flavor_loc[XDIM], flavor_loc[YDIM],  goalLayer  ] = 1.0
-                for gx,gy in goal_locs:                 st[gx, gy,  mobileLayer] = 1.0
-                for x in range(self.gridsize[XDIM]):    st[x, ry, immobileLayer] = 1.0
-                for y in range(self.gridsize[YDIM]):    st[rx, y, immobileLayer] = 1.0
-                for _x,_y in start_locs:                st[_x, _y, immobileLayer] = 0.0
+                st = np.zeros( (self.gridsize[X], self.gridsize[Y], NUM_LAYERS))
+                st[ start_box[X],  start_box[Y],   agentLayer ] = 1.0
+                st[ flavor_loc[X], flavor_loc[Y],  goalLayer  ] = 1.0
+                for mx,my in mobile_locs:
+                    st[ mx, my, mobileLayer  ] = 1.0
+                for bx,by in block_locs:
+                    st[ bx, by, immobileLayer  ] = 1.0
+
+#                for gx,gy in goal_locs:                 st[gx, gy,  mobileLayer] = 1.0
+#                for x in range(self.gridsize[XDIM]):    st[x, ry, immobileLayer] = 1.0
+#                for y in range(self.gridsize[YDIM]):    st[rx, y, immobileLayer] = 1.0
+#                for _x,_y in start_locs:                st[_x, _y, immobileLayer] = 0.0
                 self.start_states.append( { 'signal': flav_id,
                     'state': st})
+#            print self.start_states[-1]['state'][:,:,agentLayer]
+        rnd_state = self.start_states[np.random.choice(range(24))]
+        print_state(rnd_state, 'condensed')
 
 
 for centr in ['egocentric', 'allocentric']:
